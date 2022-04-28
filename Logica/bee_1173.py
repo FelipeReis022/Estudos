1ea3e05bf431d5208f8cfa918c19cca94d79d9aa
FelipeReis <freis013@gmail.com>
2022-04-28 09:15:33 -0300

    Dobra valor de entrada

---
+++ Logica/bee_1173.py
@@ -0,0 +1,28 @@
+def dobra_valor(num) -> list:
+    """Recebe um valor inicial e cria uma lista
+    dobrando os respectivos valores."""
+    lista_valores = []
+    lista_valores.append(num)
+    for n in lista_valores:
+        lista_valores.append(n*2)
+        if len(lista_valores) == 10:
+            break
+    return lista_valores
+
+def imprime_lista(num) -> str:
+    posicao = 0
+    for n in dobra_valor(num):
+        print(f'N[{posicao}] = {n}')
+        posicao += 1
+
+numero = int(input())
+imprime_lista(numero)
+
+# for n in lista:
+#     lista.append(n*2)
+#     if len(lista) == 10:
+#         break
+
+# for n in lista:
+#     print(f'N[{posicao}] = {n}')
+#     posicao += 1
