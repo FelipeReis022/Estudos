378f04b5e30e6bdb2077421d05cb79c0cbda05e0
Felipe reis <felipereis@pop-os.localdomain>
2022-05-02 09:46:07 -0300

    Retorna lista com 1000 valores e seu index

--- Logica/bee_1177.py
+++ Logica/bee_1177.py
@@ -1,11 +1,11 @@
 numero = int(input())
 lista = list(range(0, numero))
-cont = 0
+index = 0
 
 for n in lista:
     lista.append(n)
     if len(lista) == 1000:
         break
 for n in lista:
-    print(f'N[{cont}] = {n}')
-    cont += 1
+    print(f'N[{index}] = {n}')
+    index += 1
