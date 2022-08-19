dbf5380eaedec781dc7b0980829c75e33b41cc5f
Felipe reis <freis013@gmail.com>
2022-08-19 14:50:05 -0300

    Ternario

---
+++ Fundamentos-Javascript/operadorTernario.js
@@ -0,0 +1,12 @@
+const idadeMinima = 18;
+const idadeCliente = 15;
+
+//If normal
+if (idadeCliente >= idadeMinima) {
+    console.log("Cerveja")
+} else {
+    console.log("Suco")
+}
+
+//Ternário - Condição do if               //true     //false
+console.log(idadeCliente >= idadeMinima ? "Cerveja" : "Suco")
